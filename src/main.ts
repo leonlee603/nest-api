48c4264447aadc740ff65d7edb1a13a47f58134d
Leon Lee <leepingho603@gmail.com>
2026-03-08 23:24:08 +0800

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configure the Swagger UI
+  const config = new DocumentBuilder()
+    .setTitle('Nest API')
+    .setDescription('Nest API description')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000', 'Local')
+    // .addServer('https://api.nestjs.com', 'Production')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Set 'api' as the path for the Swagger UI
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
