2b5dde87f8d44f370311dde40526ceac780b0e70
Leon Lee <leepingho603@gmail.com>
2026-03-10 20:07:20 +0800

    Update meta-option entity

--- src/meta-options/entities/meta-option.entity.ts
+++ src/meta-options/entities/meta-option.entity.ts
@@ -1,1 +1,22 @@
-export class MetaOption {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class MetaOption {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'json', nullable: false })
+  metaValue: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
